b8a36566281a7f73d70db188fa9ff48cadd6894c
mik14a <mik14a@gmail.com>
2023-12-04 04:53:06 +0900

    Inject dependencies via Inspector instead of using FindObjectOfType

--- SepiaStock/Assets/Scripts/Controllers/ConfigController.cs
+++ SepiaStock/Assets/Scripts/Controllers/ConfigController.cs
@@ -1,32 +1,37 @@
 using SepiaStock.Unity.ObservableModels;
 using SepiaStock.Unity.Presenters;
-using SepiaStock.Views;
 
 using UnityEngine;
 
 /// <summary>
-/// 設定コントローラークラス
+/// 設定コントローラークラスです。設定画面の制御を担当します。
 /// </summary>
 class ConfigController : MonoBehaviour
 {
+    /// <summary>設定ビュー</summary>
+    [SerializeField] ConfigView _view;
+
+    /// <summary>初期化時に呼び出されます。</summary>
     void Awake()
     {
         _model = ConfigModel.CreateInstance();
-        _view = FindObjectOfType<ConfigView>();
         _presenter = ConfigPresenter.CreateInstance(_model, _view);
         _presenter.OnBack += Back;
     }
 
+    /// <summary>開始時に呼び出されます。</summary>
     void Start()
     {
         _presenter.Initialize();
     }
 
+    /// <summary>毎フレーム更新時に呼び出されます。</summary>
     void Update()
     {
         _commandManager.Execute();
     }
 
+    /// <summary>戻るコマンドを実行します。</summary>
     void Back()
     {
         _commandManager.Push(new BackCommand());
@@ -35,7 +40,8 @@ class ConfigController : MonoBehaviour
     /// <summary>コマンドマネージャー</summary>
     readonly CommandManager _commandManager = new();
 
+    /// <summary>設定モデル</summary>
     ConfigModel _model;
-    IConfigView _view;
+    /// <summary>設定プレゼンター</summary>
     ConfigPresenter _presenter;
 }
--- SepiaStock/Assets/Scripts/Controllers/RootController.cs
+++ SepiaStock/Assets/Scripts/Controllers/RootController.cs
@@ -1,42 +1,49 @@
 using SepiaStock.Unity.Presenters;
-using SepiaStock.Views;
 
 using UnityEngine;
 
 /// <summary>
-/// ルートコントローラークラス
+/// ルートコントローラークラスです。アプリケーションのルート制御を担当します。
 /// </summary>
 class RootController : MonoBehaviour
 {
+    /// <summary>ルートビュー</summary>
+    [SerializeField] RootView _view;
+
+    /// <summary>初期化時に呼び出されます。</summary>
     void Awake()
     {
-        _view = FindObjectOfType<RootView>();
         _presenter = RootPresenter.CreateInstance(_view);
         _presenter.OnPhotoSelect += PhotoSelect;
         _presenter.OnCreateAlbum += CreateAlbum;
         _presenter.OnConfig += Config;
     }
 
+    /// <summary>開始時に呼び出されます。</summary>
     void Start()
     {
         _presenter.Initialize();
     }
 
+    /// <summary>更新時に呼び出されます。</summary>
     void Update()
     {
         _commandManager.Execute();
     }
 
+    /// <summary>写真選択時に呼び出されます。</summary>
     void PhotoSelect()
     {
         _commandManager.Push(new PhotoSelectCommand());
     }
 
+    /// <summary>アルバム作成時に呼び出されます。</summary>
     void CreateAlbum()
     {
         _commandManager.Push(new CreateAlbumCommand());
     }
 
+    /// <summary>設定時に呼び出されます。</summary>
     void Config()
     {
         _commandManager.Push(new ConfigCommand());
@@ -47,6 +54,6 @@ class RootController : MonoBehaviour
     /// </summary>
     readonly CommandManager _commandManager = new();
 
-    IRootView _view;
+    /// <summary>ルートプレゼンター</summary>
     RootPresenter _presenter;
 }
